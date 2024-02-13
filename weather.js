c2d9a6825fee38de7485184731fa3f327679fc4c
zahrabateninia <zahrabatenin@gmail.com>
2024-02-13 16:23:52 +0330

    Make the search box work, diplay the cities weather info

--- weather.js
+++ weather.js
@@ -1,18 +1,21 @@
 const apiKey= "c1f6ccd45ae9a74e6f120e0bc745a4b8"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Isfahan";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+async function checkWeather(city){
+    const response = await fetch(apiUrl + `&q=${city}&appid=${apiKey}`)
     const data = await response.json();
-    // changeCardUI()
+
     document.querySelector(".city-name").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector('.description').innerHTML = data.weather[0].description;
-    document.querySelector('.humidity-value').innerHtml = data.main.humidity + "%";
-    document.querySelector(".windSpeed-value").innerHtml = data.wind.speed + "km/h";
-
-    console.log(data)
+    document.querySelector(".humidity-value").textContent = data.main.humidity + "%";
+    document.querySelector(".windSpeed-value").textContent = data.wind.speed + "km/h";
  
 }
 
-checkWeather()
+const cityName = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search-btn');
+
+searchBtn.addEventListener('click', ()=> {
+    checkWeather(cityName.value)
+})
