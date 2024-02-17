4686b8c5d15871ba30022766584b199b1aa268eb
zahrabateninia <zahrabatenin@gmail.com>
2024-02-17 17:20:06 +0330

    Change the weather icon and background image based on the weather condition

--- weather.js
+++ weather.js
@@ -2,8 +2,9 @@ const apiKey= "c1f6ccd45ae9a74e6f120e0bc745a4b8"
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
 async function checkWeather(city){
-
+    // add loading
     const response = await fetch(apiUrl + `&q=${city}&appid=${apiKey}`);
+    // close loading
     if(response.status == 404){
         showErrorMsg()
         return;
@@ -27,7 +28,6 @@ searchBtn.addEventListener('click', ()=> {
 })
 
 function updateUI(data){
-
     document.querySelector(".city-name").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector('.description').innerHTML = data.weather[0].description;
@@ -41,33 +41,62 @@ function updateUI(data){
 function changeWeatherIconAndBg(data){
     // based on weather condition change the icon
     const weatherIcon = document.querySelector(".weather-icon")
-    document.body.style.backgroundImage = ""
 
     if( data.weather[0].main == "Clouds"){
         document.body.style.backgroundImage = "url(weatherImages/cloudy.jpg)"
         document.body.style.backgroundSize = "cover"
-        weatherIcon.src = "weatherImages/weather icons/cloudy_1163661.png"
+        weatherIcon.src = "weatherImages/weather icons/cloudySunny.png"
     }
     else if(data.weather[0].main == "Clear"){
         document.body.style.backgroundImage = "url(weatherImages/sunny.jpg)"
         document.body.style.backgroundSize = "cover"
-        weatherIcon.src = "weatherImages/weather icons/sun_869869.png "
+        weatherIcon.src = "weatherImages/weather icons/sunny.png"
     }
     else if(data.weather[0].main == "Rain"){
         document.body.style.backgroundImage = "url(weatherImages/rainy.jpg)"
         document.body.style.backgroundSize = "cover"
-        weatherIcon.src = "weatherImages/weather icons/cloudy_1163661.png"
+        weatherIcon.src = "weatherImages/weather icons/rainy.png"
     }
     else if(data.weather[0].main == "Drizzle"){
         document.body.style.backgroundImage = "url(weatherImages/rainy.jpg)"
         document.body.style.backgroundSize = "cover"
-        weatherIcon.src = "weatherImages/weather icons/cloudy_1163661.png"
+        weatherIcon.src = "weatherImages/weather icons/drizzle.png"
     }
-    else if(data.weather[0].main == "Mist"){
+    else if(data.weather[0].main == "Haze"|| data.weather[0].main == "Smoke" || data.weather[0].main == "Dust"){ 
+        // haze: dust and smoke in the air
         document.body.style.backgroundImage = "url(weatherImages/haze.jpg)"
         document.body.style.backgroundSize = "cover"
-        weatherIcon.src = "weatherImages/weather icons/cloudy_1163661.png"
+        weatherIcon.src = "weatherImages/weather icons/mist.png"
     }
+    else if(data.weather[0].main == "Snow"){
+        document.body.style.backgroundImage = "url(weatherImages/snow.jpg)"
+        document.body.style.backgroundSize = "cover"
+        weatherIcon.src = "weatherImages/weather icons/snowy.png"
+    }
+    else if(data.weather[0].main == "Mist" || data.weather[0].main == "Fog"){
+        document.body.style.backgroundImage = "url(weatherImages/mistFog.jpg)"
+        document.body.style.backgroundSize = "cover"
+        weatherIcon.src = "weatherImages/weather icons/mist.png"
+    }
+    else if(data.weather[0].main == "Tornado"){
+        document.body.style.backgroundImage = "url(weatherImages/tornado.jpg)"
+        document.body.style.backgroundSize = "cover"
+        weatherIcon.src = "weatherImages/weather icons/tornado.png"
+    }
+    else if(data.weather[0].main == "Thunderstorm"){
+        document.body.style.backgroundImage = "url(weatherImages/storm.jpg)"
+        document.body.style.backgroundSize = "cover"
+        weatherIcon.src = "weatherImages/weather icons/stormy.png"
+    }
+    else if(data.weather[0].main == "Wind"){
+        document.body.style.backgroundImage = "url(weatherImages/windyClear.jpg)"
+        document.body.style.backgroundSize = "cover"
+        weatherIcon.src = "weatherImages/weather icons/windy.png"
+    }
+    
+    
+    
+    
     
 
 }
